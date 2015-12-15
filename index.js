b14abb3ed2608b9013c9f2b28b52d23c3447950d
Sohrab Aboozarkhani Fard <sohrab.a.fard@gmail.com>
2015-12-16 00:24:08 +0330

    use filter instead of action!

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
     var parser,
         runScript = {
             parsePost: function(data, callback) {
-                //if (data && data.postData && data.postData.content) {
-                //    //data.postData.content = parser.render(data.postData.content);
-                //    console.log(data.postData.pid);
-                //}
+                if (data && data.postData && data.postData.content) {
+                    //data.postData.content = parser.render(data.postData.content);
+                    console.log(data.postData.pid);
+                }
                 callback(null, data);
                 //Post.getPidIndex(data.pid, data.uid, function(err, index) {
                 //
@@ -19,24 +19,6 @@
                 //    //}
                 //
                 //});
-            },
-            savePost :  function (data, callback) {
-                var limit = 45;
-                console.log("savePost");
-
-                //Post.getPidIndex(data.pid, data.uid, function(err, index) {
-                //
-                //    console.log(index);
-                //    console.warn("actions-alter");
-                //    //if( !err && index >= limit )
-                //    //{   // if no error and the index >= limit -> LOCK
-                //    //    console.log(data);
-                //    //    console.log(err);
-                //    //}
-                //
-                //});
-
-                callback(null, data);
             }
     };
     module.exports = runScript;
