8753f76b8e87752a2b0b7592a6c0720a6b5aeffa
Sohrab Aboozarkhani Fard <sohrab.a.fard@gmail.com>
2015-12-15 18:39:15 +0330

    change library

--- library.js
+++ library.js
@@ -1,35 +1,39 @@
 "use strict";
 
-var User = module.parent.require('./user');
-var Topic = module.parent.require('./topics');
-var ThreadTools = module.parent.require('./threadTools');
-var Post = module.parent.require('./posts');
-var SocketAdmins = module.parent.require('./socket.io/admin');
-var db = module.parent.require('./database');
-
-var Utils = module.parent.require('../public/src/utils')
-
-var plugin = {};
+var User = module.parent.require('./user'),
+    Topic = module.parent.require('./topics'),
+    ThreadTools = module.parent.require('./threadTools'),
+    Post = module.parent.require('./posts'),
+    SocketAdmins = module.parent.require('./socket.io/admin'),
+    db = module.parent.require('./database'),
+    Utils = module.parent.require('../public/src/utils');
 
 
-plugin.alterPost = function (data, callback) {
+(function(Plugin) {
 
-	//configure your thread limit
-    var limit = 45;
+    Plugin.hooks = {
+        filters: {},
+        statics: {},
+        actions: {
+            alter : function (data, callback) {
+                var limit = 45;
+                Post.getPidIndex(data.pid, data.uid, function(err, index) {
 
-    //console.log(data);
-    // ThreadTools.lock(topicObj.tid, 0, next);
-    Post.getPidIndex(data.pid, data.uid, function(err, index){
-        //console.log(index);
-        if( !err && index >= limit )
-        {   // if no error and the index >= limit -> LOCK
-            console.log(data);
-            console.log(err);
+                    console.log(index);
+
+                    if( !err && index >= limit )
+                    {   // if no error and the index >= limit -> LOCK
+                        console.log(data);
+                        console.log(err);
+                    }
+
+                });
+
+            }
         }
-    });
-};
+    };
 
-module.exports = plugin;
+})(module.exports);
 
 
 
