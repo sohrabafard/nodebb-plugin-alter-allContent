5d39c6eef788405385c2b55e8d91eb3b8a13534f
Sohrab Aboozarkhani Fard <sohrab.a.fard@gmail.com>
2015-12-15 21:01:50 +0330

    add fillter

--- library.js
+++ library.js
@@ -1,14 +1,13 @@
-"use strict";
-
-var User = module.parent.require('./user'),
-    Topic = module.parent.require('./topics'),
-    Post = module.parent.require('./posts'),
-    SocketAdmins = module.parent.require('./socket.io/admin'),
-    db = module.parent.require('./database'),
-    Utils = module.parent.require('../public/src/utils');
-
-
 (function(Plugin) {
+
+    "use strict";
+
+    var User = module.parent.require('./user'),
+        Topic = module.parent.require('./topics'),
+        Post = module.parent.require('./posts'),
+        SocketAdmins = module.parent.require('./socket.io/admin'),
+        db = module.parent.require('./database'),
+        Utils = module.parent.require('../public/src/utils');
 
     Plugin.alter = function (data, callback) {
         var limit = 45;
