21f85b06d380ee9d80270d88e835831e2f0450be
Sohrab Aboozarkhani Fard <sohrab.a.fard@gmail.com>
2015-12-16 00:10:54 +0330

    .

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 (function() {
     "use strict";
-    var User = module.parent.require('./user'),
-        Topic = module.parent.require('./topics'),
-        Post = module.parent.require('./posts'),
-        SocketAdmins = module.parent.require('./socket.io/admin'),
-        db = module.parent.require('./database'),
-        Utils = module.parent.require('../public/src/utils');
-
     var parser,
         runScript = {
             parsePost: function(data, callback) {
@@ -31,17 +24,17 @@
                 var limit = 45;
                 console.log("savePost");
 
-                Post.getPidIndex(data.pid, data.uid, function(err, index) {
-
-                    console.log(index);
-                    console.warn("actions-alter");
-                    //if( !err && index >= limit )
-                    //{   // if no error and the index >= limit -> LOCK
-                    //    console.log(data);
-                    //    console.log(err);
-                    //}
-
-                });
+                //Post.getPidIndex(data.pid, data.uid, function(err, index) {
+                //
+                //    console.log(index);
+                //    console.warn("actions-alter");
+                //    //if( !err && index >= limit )
+                //    //{   // if no error and the index >= limit -> LOCK
+                //    //    console.log(data);
+                //    //    console.log(err);
+                //    //}
+                //
+                //});
 
                 callback(null, data);
             }
