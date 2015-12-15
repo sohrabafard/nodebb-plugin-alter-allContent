a1fb7ee9ea79de78096478ab6f0607fdf365943d
Sohrab Aboozarkhani Fard <sohrab.a.fard@gmail.com>
2015-12-16 01:39:23 +0330

    .

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+var pkg = require('../package.json'),
+    fs = require('fs-extra'),
+    _ = require('underscore'),
+    path = require('path'),
+
+    winston = module.parent.require('winston'),
+    nconf = module.parent.require('nconf'),
+    async = module.parent.require('async'),
+    meta = module.parent.require('./meta'),
+    sockets = module.parent.require('./socket.io'),
+    utils = require(path.join(__dirname, '/../public/js/utils.js'));
 (function (Plugin) {
     'use strict';
+    console.log("Alter Plugin");
     //NodeBB list of Hooks: https://github.com/NodeBB/NodeBB/wiki/Hooks
     Plugin.hooks = {
         filters: {
